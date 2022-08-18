9923a966a79b7a3debb70ae3276f0a63c9dad28b
Paige Sylvan <paigesylvan@gmail.com>
2022-08-19 00:07:28 +0200

    initial commit

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ The function should:
 */
 
 
-function createMenuItem(/*Your code here*/){
-  /*Your code here*/
+function createMenuItem(name, price, category){
+  return  {name, price, category}
 }
 
 
